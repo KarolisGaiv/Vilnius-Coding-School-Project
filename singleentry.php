7a76968d4746a599c015d3ef84c1d194d9f90bb9
vaidmis <vaidmis@gmail.com>
2020-04-23 21:44:25 +0300

    Toliau dirbu su Single entry puslapiu

--- singleentry.php
+++ singleentry.php
@@ -16,22 +16,24 @@
 
 <h2 class="center-align">Single entry</h2>
 
-<div class="row center-align">
-    <div class="col s12">
-        <img src="https://picsum.photos/seed/1/1000/600" alt="">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa officiis cupiditate laboriosam nam, itaque a soluta ducimus! Autem suscipit ut natus nihil odio delectus voluptatum. Totam autem cum placeat. lorem</p>
-    </div>
 
-    <div class="col s6">
-        <img src="https://picsum.photos/seed/2/1000/600" alt="">
+<div class="main-news">
+    <img src="https://picsum.photos/seed/1/500/300" alt="">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa officiis cupiditate laboriosam nam, itaque a soluta ducimus! Autem suscipit ut natus nihil odio delectus voluptatum. Totam autem cum placeat. lorem</p>
+</div>
+
+<div class="news">
+    <div>
+        <img src="https://picsum.photos/seed/2/500/300" alt="">
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nobis nemo vel doloremque quae corporis aut, consequuntur assumenda neque cumque dolores delectus hic officiis totam non. Aperiam mollitia quaerat repudiandae.</p>
     </div>
 
-    <div class="col s6">
-        <img src="https://picsum.photos/seed/3/1000/600" alt="">
+    <div>
+        <img src="https://picsum.photos/seed/3/500/300" alt="">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At magnam debitis quam itaque corporis temporibus impedit eum. Voluptates facere magnam maiores ab commodi consequatur officiis, eveniet, repellat aut eligendi distinctio.</p>
     </div>
 </div>
+
 
 
 
