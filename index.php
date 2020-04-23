816e2adbbb7f3d49774f3988a3cd81437fcd57d5
KarolisGaiv <kgaivenis@gmail.com>
2020-04-23 20:25:40 +0300

    Pridėti trys mygtukai

--- index.php
+++ index.php
@@ -28,27 +28,26 @@
 
       <div class="col s4">
         <img src="https://picsum.photos/300/300" alt="Left Picture">
+        <p class="introLabels">Some text</p>
+        <a class="btn" href="contacts.php">Button</a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est. Enim similique minus nobis quam placeat? Deserunt quis autem ab et labore, distinctio optio laudantium impedit, velit, dicta aut amet.</p>
       </div>
       <div class="col s4">
         <img src="https://picsum.photos/301/300" alt="Middle Picture">
+        <p class="introLabels">Some text</p>
+        <a class="btn" href="contacts.php">Button</a>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime minima optio quibusdam laudantium? At omnis, enim nesciunt repudiandae necessitatibus reiciendis facilis ea neque obcaecati laborum voluptatibus? Vitae temporibus porro dicta.</p>
       </div>
       <div class="col s4">
         <img src="https://picsum.photos/302/300" alt="Right Picture">
+        <p class="introLabels">Some text</p>
+        <a class="btn" href="contacts.php">Button</a>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur sit, eaque suscipit porro consequuntur optio ipsa quam mollitia commodi architecto totam delectus vitae animi dolore iure dignissimos sint debitis! Molestias.</p>
       </div>
 
-
     </div>
 
-
-
   </div>
-
-
-
-
 
 </div>
 
