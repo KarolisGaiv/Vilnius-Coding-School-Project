44e06250bc1baf83d330c613a5d8598e6dd86697
romanrianin <roman.voronovic@gmail.com>
2020-04-23 13:51:49 +0300

    Pradejau footeri.

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
     
-    <!-- Čia pasikartojanti visuose puslapiuose apatinė dalis -->
+    <footer class="page-footer">
+		<div class="container">
+
+			<nav>
+				<ul class="footer-meniu">
+					<li><a href="index.php">Home</a></li>
+                    <li><a href="gallery.php">News</a></li>
+                    <li><a href="about.php">About Us</a></li>
+					<li><a href="contacts.php">Contact</a></li>
+				</ul>
+
+				<ul class="social-links">
+					<li><a target="blank" href="https://www.facebook.com/"><img src="images/facebook-circle.png"></a></li>
+					<li><a target="blank" href="https://twitter.com/"><img src="images/twitter-circle.png"></a></li>
+					<li><a target="blank" href="https://lt.linkedin.com/"><img src="images/linkedin-circle.png"></a></li>
+					<li><a target="blank" href="https://www.instagram.com/"><img src="images/instagram-circle.png"></a></li>
+				</ul>
+			</nav>
+		</div>
+	</footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="scripts/script.j"></script>
