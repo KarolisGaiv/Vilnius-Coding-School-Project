18c106b4ca59ebb07dadd290155c648292d3495f
vaidmis <vaidmis@gmail.com>
2020-04-24 13:44:14 +0300

    Patobulintas Single entry

--- singleentry.php
+++ singleentry.php
@@ -14,26 +14,40 @@
     </div>
 </nav>
 
-<h2 class="center-align">Single entry</h2>
+<div class="single-entry container">
+    <h2 class="center-align">Single entry</h2>
 
-
-<div class="main-news">
-    <img src="https://picsum.photos/seed/1/500/300" alt="">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa officiis cupiditate laboriosam nam, itaque a soluta ducimus! Autem suscipit ut natus nihil odio delectus voluptatum. Totam autem cum placeat. lorem</p>
-</div>
-
-<div class="news">
-    <div>
-        <img src="https://picsum.photos/seed/2/500/300" alt="">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nobis nemo vel doloremque quae corporis aut, consequuntur assumenda neque cumque dolores delectus hic officiis totam non. Aperiam mollitia quaerat repudiandae.</p>
+    <div class="main-news">
+        <img src="https://picsum.photos/seed/1/500/300" alt="">
+        <h3>Name</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa officiis cupiditate laboriosam nam, itaque a soluta ducimus! Autem suscipit ut natus nihil odio delectus voluptatum. Totam autem cum placeat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde modi blanditiis explicabo dolor sequi fugiat, accusantium consequuntur, ipsa quo consectetur labore rerum voluptas nam praesentium enim architecto reprehenderit mollitia minima. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ipsum atque dolore laudantium in deleniti id, totam temporibus voluptatem labore porro voluptates. Unde fugit temporibus quaerat asperiores. Sed, explicabo excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, eaque temporibus! Vitae impedit delectus quas distinctio quos nihil! Odit aliquid deleniti maxime earum! Reprehenderit mollitia eum sapiente, natus amet eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quis dolor aliquam harum dignissimos dicta, magnam assumenda quaerat dolore maiores laboriosam voluptates quas aut. Consequuntur, cupiditate. Facilis non odit fugit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae voluptas iusto, laboriosam iste rem sit! Id nobis repellendus quod porro? Aliquid, accusantium praesentium. Numquam explicabo dignissimos blanditiis ducimus natus consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate est mollitia distinctio sint beatae magni corporis placeat vitae ea reiciendis, minima eius pariatur veritatis repudiandae laudantium itaque veniam aliquid nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo voluptas nisi molestiae debitis dolores expedita quas consequuntur, ex eum ipsum minima itaque eligendi et necessitatibus, repudiandae soluta unde facilis ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quas laudantium pariatur architecto, itaque praesentium laboriosam necessitatibus molestias natus ducimus quis nihil repudiandae aperiam sunt, ullam iure, aliquam provident. Magnam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque sed dolores ad expedita suscipit libero consectetur, exercitationem molestiae fuga iusto distinctio nobis ab totam modi fugiat maiores unde eveniet illum!</p>
     </div>
 
-    <div>
-        <img src="https://picsum.photos/seed/3/500/300" alt="">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At magnam debitis quam itaque corporis temporibus impedit eum. Voluptates facere magnam maiores ab commodi consequatur officiis, eveniet, repellat aut eligendi distinctio.</p>
+    <div class="row news">
+        <div class="col s4 offset-2 news-small">
+                <div class="img-div">
+                    <img class="article-photo" src="https://picsum.photos/seed/2/500/300" alt="">
+                </div>
+                <div class="news-inside">
+                    <h3>Name</h3>
+                    <p class="truncate">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nobis nemo vel doloremque quae corporis aut, consequuntur assumenda neque cumque dolores delectus hic officiis totam non. Aperiam mollitia quaerat repudiandae.</p>
+                    <a href="singleentry.php">Read more...</a>
+                </div>
+        </div>
+
+
+        <div class="col s4 offset-2 news-small">
+                <div class="img-div">
+                    <img class="article-photo" src="https://picsum.photos/seed/3/500/300" alt="">
+                </div>
+                <div class="news-inside">
+                    <h3>Name</h3>
+                    <p class="truncate">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nobis nemo vel doloremque quae corporis aut, consequuntur assumenda neque cumque dolores delectus hic officiis totam non. Aperiam mollitia quaerat repudiandae.</p>
+                    <a href="singleentry.php">Read more...</a>
+                </div>
+        </div>
     </div>
 </div>
-
 
 
 
