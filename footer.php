88d9ef4357f91932400199631b50ae7695008751
romanrianin <roman.voronovic@gmail.com>
2020-04-26 18:04:01 +0300

    Footer stilius keiciau, nedraugauja su materialize

--- footer.php
+++ footer.php
@@ -5,12 +5,14 @@
             <h5 class="white-text">Connect with us</h5>
             <p class="grey-text text-lighten-4">You can use links to reach us</p>
         </div>
-        <div class="social">
-            <!-- <a class="col s3" target="blank" href="https://www.facebook.com/"><img class="footerimg" src="images/facebook-circle.png"></a>
-                        <a class="col s3" target="blank" href="https://twitter.com/"><img class="footerimg" src="images/twitter-circle.png"></a>
-                        <a class="col s3" target="blank" href="https://lt.linkedin.com/"><img class="footerimg" src="images/linkedin-circle.png"></a>
-                        <a class="col s3" target="blank" href="https://www.instagram.com/"><img class="footerimg" src="images/instagram-circle.png"></a> -->
-            <div>
+        <div class="social row">
+            <div class="col s6 m6 l6 offset-s3 offset-m3 offset-l3">
+            <a class="col s3" target="blank" href="https://www.facebook.com/"><img class="footerimg" src="images/facebook-circle.png"></a>
+            <a class="col s3" target="blank" href="https://twitter.com/"><img class="footerimg" src="images/twitter-circle.png"></a>
+            <a class="col s3" target="blank" href="https://lt.linkedin.com/"><img class="footerimg" src="images/linkedin-circle.png"></a>
+            <a class="col s3" target="blank" href="https://www.instagram.com/"><img class="footerimg" src="images/instagram-circle.png"></a>
+            </div>
+            <!-- <div>
                 <a target="blank" href="https://www.facebook.com/"><img class="footerimg" src="images/facebook-circle.png"></a>
             </div>
             <div>
@@ -21,7 +23,7 @@
             </div>
             <div>
                 <a target="blank" href="https://www.instagram.com/"><img class="footerimg" src="images/instagram-circle.png"></a>
-            </div>
+            </div> -->
         </div>
     </div>
     <div class="footer-copyright">
