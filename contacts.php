aeda4ae6892c0275fe038a7d4bcb78b2df9720bc
KarolisGaiv <kgaivenis@gmail.com>
2020-04-26 16:44:48 +0300

    Forma su POST

--- contacts.php
+++ contacts.php
@@ -13,20 +13,15 @@
     <div class="container contact">
         <h2>How to reach us?</h2>
         <p class="flow-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quasi corrupti distinctio, dolor eligendi vel consequatur quas. Velit quibusdam ratione aliquam earum incidunt fuga, quo sed esse alias nulla officiis.</p>
-
     </div>
 
-
-<!-- stumti pati zemelapi ir lyguoti su forma -->
-
-    <!-- <div class="container">      //Patikrint koks skirtumas su konteineriu ir be -->
     <div class="row">
-        <div class="col s12 l5 map">
-            <div style="width: 100%"><iframe width="100%" height="400" src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=en&amp;q=harare+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=6&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Google Maps radius calculator</a></iframe></div><br />
+        <div class="col s12 l5">
+            <div style="width: 100%; margin-top: 25px;"><iframe width="100%" height="400" src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=en&amp;q=harare+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=6&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Google Maps radius calculator</a></iframe></div><br />
         </div>
 
         <div class="col s12 l5 offset-l2 conatctForm">
-            <form action="testpost.php" method="post">
+            <form action="testpost.php" method="POST">
 
                 <div class="input-field">
                     <i class="material-icons prefix">email</i>
@@ -36,18 +31,17 @@
 
                 <div class="input-field">
                     <i class="material-icons prefix">account_box</i>
-                    <input type="text" id="fName" name="fName">
-                    <label for="fName">Your Name</label>
+                    <input type="text" id="name" name="name">
+                    <label for="name">Your Name</label>
                 </div>
 
                 <div class="input-field">
                     <i class="material-icons prefix">message</i>
-                    <textarea id="message" class="materialize-textarea"></textarea>
+                    <textarea id="message" class="materialize-textarea" name="message"></textarea>
                     <label for="message">Your message</label>
                 </div>
 
                 <div class="input-field">
-                    <!-- <i class="material-icons prefix">date_range</i> -->
                     <input type="text" id="date" class="datepicker" name="date">
                     <label for="date">Choose date when you need us</label>
                 </div>
@@ -64,14 +58,8 @@
             </form>
         </div>
     </div>
-    <!-- </div> -->
-
-
 
 </div>
-
-
-
 
 
 <?php include "footer.php" ?>
