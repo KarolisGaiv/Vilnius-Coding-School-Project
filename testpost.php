346fffa0c14bf322553e429fe96a513447132a3e
KarolisGaiv <kgaivenis@gmail.com>
2020-04-27 10:54:28 +0300

    Pataisyta duomenu baze

--- testpost.php
+++ testpost.php
@@ -12,19 +12,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $name = mysqli_real_escape_string($conn, $_POST['name']);
 $message = mysqli_real_escape_string($conn, $_POST['message']);
-// $date = mysqli_real_escape_string($conn, $_POST['date']);
+$date = mysqli_real_escape_string($conn, $_POST['date']);
+$date = date ("Y-m-d");
 
 
-$sql = "INSERT INTO contact_requests (email, name, message) VALUES (?, ?, ?)";
+$sql = "INSERT INTO contact_requests (email, name, message, date) VALUES (?, ?, ?, ?)";
 
 $stmt = mysqli_stmt_init($conn);
 if (!mysqli_stmt_prepare($stmt, $sql)) {
     echo "System error ";
 } else {
-    mysqli_stmt_bind_param($stmt, "sss", $email, $name, $message);
+    mysqli_stmt_bind_param($stmt, "ssss", $email, $name, $message, $date);
     mysqli_stmt_execute($stmt);
 }
 
