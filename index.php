a47d6481759ac87a0938c07883980b652c041bc3
romanrianin <roman.voronovic@gmail.com>
2020-04-27 19:30:22 +0300

    Pakeiciau karuseles pavadinimus

--- index.php
+++ index.php
@@ -20,28 +20,28 @@
       <div class="carousel">
         <div class="carousel-item">
           <img src="https://picsum.photos/seed/1/300/300">
-          <p class="introLabels">Some text</p>
+          <p class="introLabels">News</p>
           <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="news.php">Button</a>
         </div>
         <div class="carousel-item">
           <img src="https://picsum.photos/seed/2/300/300">
-          <p class="introLabels">Some text</p>
+          <p class="introLabels">About</p>
           <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="about.php">Button</a>
         </div>
         <div class="carousel-item">
           <img src="https://picsum.photos/seed/3/300/300">
-          <p class="introLabels">Some text</p>
+          <p class="introLabels">Contacts</p>
           <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="contacts.php">Button</a>
         </div>
         <div class="carousel-item">
           <img src="https://picsum.photos/seed/4/300/300">
-          <p class="introLabels">Some text</p>
+          <p class="introLabels">Random news</p>
           <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="singleentry.php">Button</a>
         </div>
         <div class="carousel-item">
           <img src="https://picsum.photos/seed/5/300/300">
-          <p class="introLabels">Some text</p>
-          <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="contacts.php">Button</a>
+          <p class="introLabels">Statistics</p>
+          <a class="waves-effect waves-light btn z-depth-5 waves-purple" href="statistics.php">Button</a>
         </div>
       </div>
 
